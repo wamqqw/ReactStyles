1bc26897afa3539b9e5c3bed9dfc71a67601db0d
Nure_sq15@student.itstep.kz <Nure_sq15@student.itstep.kz>
2023-10-05 20:54:49 +0600

    reactStyles

--- src/pages/FootballClub.js
+++ src/pages/FootballClub.js
@@ -44,126 +44,126 @@ const FootballClub = () => {
         <h2>Current squad</h2>
         <ul className="player-list">
           <li className="player-item">
-            <img src="ANCELOTTI.jpg" alt="Игрок 3" />
+            <img src="ANCELOTTI.jpg"/>
             <div className="player-info">
               <span className="player-name">Ancelotti</span><br/>
               <span className="player-position">Coach</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="Courtois.jpg" alt="gk" />
+            <img src="Courtois.jpg" />
             <div className="player-info">
               <span className="player-name">Courtois</span><br/>
               <span className="player-position">Goalkeeper</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="LUNIN.jpg" alt="lun" />
+            <img src="LUNIN.jpg"/>
             <div className="player-info">
               <span className="player-name">Lunin</span> <br/>
               <span className="player-position">Goalkeeper</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="MILITAO.jpg" alt="Игрок 3" />
+            <img src="MILITAO.jpg" />
             <div className="player-info">
               <span className="player-name">Militao</span> <br/>
               <span className="player-position">Defender</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="RUDIGER.jpg" alt="Игрок 3" />
+            <img src="RUDIGER.jpg" />
             <div className="player-info">
               <span className="player-name">Rudiger</span><br/> 
               <span className="player-position">Defender</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="ALABA.jpg" alt="gf" />
+            <img src="ALABA.jpg"/>
             <div className="player-info">
               <span className="player-name">Alaba</span><br/>
               <span className="player-position">Defender</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="MENDY.jpg" alt="Игрок 3" />
+            <img src="MENDY.jpg"/>
             <div className="player-info">
               <span className="player-name">Mendy</span><br/>
               <span className="player-position">Defender</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="CARVAJAL.jpg" alt="Игрок 3" />
+            <img src="CARVAJAL.jpg" />
             <div className="player-info">
               <span className="player-name">Carvajal</span><br/>
               <span className="player-position">Defender</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="KROOS.jpg" alt="Игрок 3" />
+            <img src="KROOS.jpg"/>
             <div className="player-info">
               <span className="player-name">Kroos</span><br/>
               <span className="player-position">Midfielder</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="MODRIC.jpg" alt="Игрок 3" />
+            <img src="MODRIC.jpg" />
             <div className="player-info">
               <span className="player-name">Modric</span><br/>
               <span className="player-position">Midfielder</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="VALVERDE.jpg" alt="Игрок 3" />
+            <img src="VALVERDE.jpg" />
             <div className="player-info">
               <span className="player-name">Valverde</span><br/>
               <span className="player-position">Midfielder</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="TCHOUAMENI.jpg" alt="Игрок 3" />
+            <img src="TCHOUAMENI.jpg" />
             <div className="player-info">
               <span className="player-name">Tchouameni</span><br/>
               <span className="player-position">Midfielder</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="BRAHIM.jpg" alt="Игрок 3" />
+            <img src="BRAHIM.jpg"/>
             <div className="player-info">
               <span className="player-name">Brahim</span><br/>
               <span className="player-position">Midfielder</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="BELLINGHAM.jpg" alt="Игрок 3" />
+            <img src="BELLINGHAM.jpg" />
             <div className="player-info">
               <span className="player-name">Bellingham</span><br/>
               <span className="player-position">Midfielder</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="CAMAVINGA.jpg" alt="Игрок 3" />
+            <img src="CAMAVINGA.jpg" />
             <div className="player-info">
               <span className="player-name">Camavinga</span><br/>
               <span className="player-position">Midfielder</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="VINICIUS.jpg" alt="Игрок 3" />
+            <img src="VINICIUS.jpg"/>
             <div className="player-info">
               <span className="player-name">Vinicius JR</span><br/>
               <span className="player-position">Forward</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="JOSELU.jpg" alt="Игрок 3" />
+            <img src="JOSELU.jpg"/>
             <div className="player-info">
               <span className="player-name">Joselu</span><br/>
               <span className="player-position">Forward</span>
             </div>
           </li>
           <li className="player-item">
-            <img src="RODRYGO.jpg" alt="Игрок 3" />
+            <img src="RODRYGO.jpg"/>
             <div className="player-info">
               <span className="player-name">Rodrygo</span><br/>
               <span className="player-position">Forward</span>
